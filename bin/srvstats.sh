b820dbfb21c5abab0f0c52bc2bb41104049d10b5
jankka <jan.matilainen@gmail.com>
2013-08-07 11:16:02 +0000

    Added startscript

---
+++ bin/srvstats.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Welcome to srvstats!"
+echo "Script starts up web service"
+echo "on ports:" 
+echo "8880 (for top information in HTML-table)" 
+echo "8888 (for application specific CPU information printed out per request as json -string)"
+
+../src/pyWebSite.py
