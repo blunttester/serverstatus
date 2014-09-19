74850efdf6befe1779fa699a2b891a3e916ad6aa
jankka <jan.matilainen@gmail.com>
2014-09-20 01:14:38 +0200

    MOdified small stuff after playing around with pylint

--- src/pySrvStats.py
+++ src/pySrvStats.py
@@ -15,35 +15,34 @@ class SrvStats(object):
         '''
         Constructor
         '''
-        
-    def cpu(self,appname):
+    def cpu(self, appname):
         self.appname = appname
         """Return float containing CPU -usage used by defined application name."""
-        
-        self.process = subprocess.Popen("ps -o pcpu -p `ps -ef | grep -i %s | grep -v grep |grep -v Users| awk '{ print $2 }'`" % self.appname,
+
+        self.process = subprocess.Popen("ps -o pcpu -p `ps -ef | grep -i %s | grep -v grep |grep -v Users| awk '{ print $2 }'`"
+                                        % self.appname,
                                         shell=True,
                                         stdout=subprocess.PIPE,
                                         )
         self.cpu = self.process.communicate()[0].split('\n')
         #print self.cpu
-        if len(self.cpu) >= 1 :
+        if len(self.cpu) >= 1:
             return float(self.cpu[1])
         else:
             return "NO CPU INFO FOR " + str(self.appname)
-    
     def top(self):
         """OS X TOP Uses different syntax and parameters than linux"""
-        self.process = subprocess.Popen("top -stats pid,rsize,vsize,cpu,th,pstate,time,command -o cpu -O +rsize -s 2 -n 10 -l 2| grep -A10 PID",
-###     Linux top -line is commented. Uncomment in case running in Linux
-#        self.process = subprocess.Popen("top -b -n 2 | grep -A10 PID",
+        #self.process = subprocess.Popen("top -stats pid,rsize,vsize,cpu,th,pstate,time,command -o cpu -O +rsize -s 2 -n 10 -l 2| grep -A10 PID",
+#     Linux top -line is commented. Uncomment in case running in Linux
+        self.process = subprocess.Popen("top -b -n 2 | grep -A10 PID",
                                         shell=True,
                                         stdout=subprocess.PIPE)
         self.top = self.process.communicate()[0].split('\n')
-        """ Since the first captured top output does not contain the wanted data, we remove the first 13 elements""" 
+        """ Since the first captured top output does not contain the wanted data, we remove the first 13 elements"""
         for x in xrange(12):
             del self.top[0]
         return self.top
-        
+
     def appTop(self,appname):
         self.appname = appname
         #print self.appname
@@ -51,9 +50,9 @@ class SrvStats(object):
                                         shell=True,
                                         stdout=subprocess.PIPE)
         self.appPid = self.app.communicate()[0].split()
-        
+
         #print self.appPid
-        
+
         self.process = subprocess.Popen("top -pid %s -stats pid,rsize,vsize,cpu,th,pstate,time,command -o cpu -O +rsize -s 2 -n 1 -l 2| grep -A10 PID" % self.appPid[1],
 ###     Linux top -line is commented. Uncomment in case running in Linux
 #        self.process = subprocess.Popen("top -p%s -b -n 1 | grep -A10 PID" % self.appPid[1],
@@ -62,25 +61,26 @@ class SrvStats(object):
         self.appTop = self.process.communicate()[0].split('\n')
         #print self.appTop[1]
         return self.appTop[1]
-    
+
     def mem(self, appname):
         self.appname = appname
-        
+
         self.app = subprocess.Popen("ps -ef | grep -i %s | awk '{ print $2 }'" % self.appname,
                                         shell=True,
                                         stdout=subprocess.PIPE)
         self.appPid = self.app.communicate()[0].split('\n')
         #print self.appPid[0]
-        
+
+        """Linux top -line is commented. Uncomment in case running in Linux"""
         self.meminfo = subprocess.Popen("top -pid %s -stats rsize,vsize -s 2 -n 1 -l 2| grep -A2 RSIZE" % self.appPid[0],
-###     Linux top -line is commented. Uncomment in case running in Linux
+
 #        self.meminfo = subprocess.Popen("top -p%s -b -n 1| grep -A2 PID" % self.appPid[0],
                                         shell=True,
                                         stdout=subprocess.PIPE)
         self.appMem = self.meminfo.communicate()[0].split('\n')
         #print self.appMem
         return self.appMem[1]
-                
+
     def threads(self, appname):
         self.appname = appname
         self.app = subprocess.Popen("ps -ef | grep -i %s | awk '{ print $2 }'" % self.appname,
@@ -88,12 +88,12 @@ class SrvStats(object):
                                         stdout=subprocess.PIPE)
         self.appPid = self.app.communicate()[0].split('\n')
         #print self.appPid[0]
-        
+
         self.threadInfo = subprocess.Popen("lsof -p %s | wc -l" % self.appPid[0],
                                         shell=True,
                                         stdout=subprocess.PIPE)
         self.appThreads = self.threadInfo.communicate()[0].split('\n')
         myThreads = str(self.appThreads[0]).split()
-        
+
         return myThreads[0]
-        
+
