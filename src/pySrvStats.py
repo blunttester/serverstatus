91a6f7fd83392e42f445ab8e291da77c227c9ef7
jankka <jan.matilainen@gmail.com>
2014-10-11 10:08:57 +0200

    Changed the process catching lines to use linux and added few comments.

--- src/pySrvStats.py
+++ src/pySrvStats.py
@@ -31,10 +31,12 @@ class SrvStats(object):
         else:
             return "NO CPU INFO FOR " + str(self.appname)
     def top(self):
-        """OS X TOP Uses different syntax and parameters than linux"""
-        #self.process = subprocess.Popen("top -stats pid,rsize,vsize,cpu,th,pstate,time,command -o cpu -O +rsize -s 2 -n 10 -l 2| grep -A10 PID",
-#     Linux top -line is commented. Uncomment in case running in Linux
-        self.process = subprocess.Popen("top -b -n 2 | grep -A10 PID",
+        """ Returns 10 most memory consuming applications on the host """
+        """ OS X TOP Uses different syntax and parameters than linux"""
+
+        self.process = subprocess.Popen("top -stats pid,rsize,vsize,cpu,th,pstate,time,command -o cpu -O +rsize -s 2 -n 10 -l 2| grep -A10 PID",
+#     OSX top -line is commented. Uncomment in case running in OSX
+        #self.process = subprocess.Popen("top -b -n 2 | grep -A10 PID",
                                         shell=True,
                                         stdout=subprocess.PIPE)
         self.top = self.process.communicate()[0].split('\n')
@@ -44,18 +46,20 @@ class SrvStats(object):
         return self.top
 
     def appTop(self,appname):
+                """ Returns TOP -information from the system """
+                """ PID,USER      PR  NI  VIRT  RES  SHR S  %CPU %MEM    TIME+  COMMAND"""
+                """ @TODO: Change the way to get the pid. There should be function for that"""
         self.appname = appname
         #print self.appname
         self.app = subprocess.Popen("ps -ef | grep -i %s " % self.appname,
                                         shell=True,
                                         stdout=subprocess.PIPE)
         self.appPid = self.app.communicate()[0].split()
-
         #print self.appPid
 
-        self.process = subprocess.Popen("top -pid %s -stats pid,rsize,vsize,cpu,th,pstate,time,command -o cpu -O +rsize -s 2 -n 1 -l 2| grep -A10 PID" % self.appPid[1],
-###     Linux top -line is commented. Uncomment in case running in Linux
-#        self.process = subprocess.Popen("top -p%s -b -n 1 | grep -A10 PID" % self.appPid[1],
+###        self.process = subprocess.Popen("top -pid %s -stats pid,rsize,vsize,cpu,th,pstate,time,command -o cpu -O +rsize -s 2 -n 1 -l 2| grep -A10 PID" % self.appPid[1],
+###     OSX top -line is commented. Uncomment in case running in OSX
+        self.process = subprocess.Popen("top -p%s -b -n 1 | grep -A10 PID" % self.appPid[1],
                                         shell=True,
                                         stdout=subprocess.PIPE)
         self.appTop = self.process.communicate()[0].split('\n')
@@ -71,10 +75,10 @@ class SrvStats(object):
         self.appPid = self.app.communicate()[0].split('\n')
         #print self.appPid[0]
 
-        """Linux top -line is commented. Uncomment in case running in Linux"""
-        self.meminfo = subprocess.Popen("top -pid %s -stats rsize,vsize -s 2 -n 1 -l 2| grep -A2 RSIZE" % self.appPid[0],
+        """OSX top -line is commented. Uncomment in case running in  OSX"""
+#        self.meminfo = subprocess.Popen("top -pid %s -stats rsize,vsize -s 2 -n 1 -l 2| grep -A2 RSIZE" % self.appPid[0],
 
-#        self.meminfo = subprocess.Popen("top -p%s -b -n 1| grep -A2 PID" % self.appPid[0],
+        self.meminfo = subprocess.Popen("top -p%s -b -n 1| grep -A2 PID" % self.appPid[0],
                                         shell=True,
                                         stdout=subprocess.PIPE)
         self.appMem = self.meminfo.communicate()[0].split('\n')
